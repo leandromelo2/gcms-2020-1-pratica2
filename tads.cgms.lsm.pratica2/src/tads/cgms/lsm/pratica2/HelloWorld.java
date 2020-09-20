2a18ac869eafa41d8a0330e393402c0899fec819
melo <melo@I-N4050>
2020-09-20 18:53:12 -0300

    commit 3

--- tads.cgms.lsm.pratica2/src/tads/cgms/lsm/pratica2/HelloWorld.java
+++ tads.cgms.lsm.pratica2/src/tads/cgms/lsm/pratica2/HelloWorld.java
@@ -3,7 +3,8 @@ package tads.cgms.lsm.pratica2;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Oĺa Mundo! teste com git!!!");
+		System.out.println("Oĺa Mundo!");
+		System.out.println("teste com git!!!");
 
 	}
 
