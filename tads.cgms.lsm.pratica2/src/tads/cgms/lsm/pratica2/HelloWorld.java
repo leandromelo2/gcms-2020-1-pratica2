d65e0c8d014e4ba8f70df1dc666c1d92fb7583e1
melo <melo@I-N4050>
2020-09-20 22:07:07 -0300

    Commit 4

--- tads.cgms.lsm.pratica2/src/tads/cgms/lsm/pratica2/HelloWorld.java
+++ tads.cgms.lsm.pratica2/src/tads/cgms/lsm/pratica2/HelloWorld.java
@@ -5,7 +5,12 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Oĺa Mundo!");
 		System.out.println("teste com git!!!");
+		 int i = 0;
+		    while (i < 5) {
+		      System.out.println(i);
+		      i++;
 
+		    }
 	}
 
 }
