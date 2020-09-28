3e6b49383fcff18fa063d8bc8c4c18066a6171f5
lsmads <lsm@a.recife.ifpe.edu.br>
2020-09-28 09:02:17 -0300

    branch hotfix

--- tads.cgms.lsm.pratica2/src/tads/cgms/lsm/pratica2/HelloWorld.java
+++ tads.cgms.lsm.pratica2/src/tads/cgms/lsm/pratica2/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Oĺa Mundo!");
 		System.out.println("gcms -git pratica2");
+		System.out.println("branch hotfix - nova ramificação");
 		System.out.println("meu repositorio");
 		
 		System.out.println("!!!");
